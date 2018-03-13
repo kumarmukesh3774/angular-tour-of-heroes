1e4fb4767c55dd591a2830577a73029ce5b0e2a8
kumarmukesh3774 <kumarmukesh3774@gmail.com>
2018-03-13 18:43:04 +0530

    in middle of service implementation ,subscribe in Heroescomponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
+//import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-//   hero :Hero = {
-//     id: 1,
-//     name: 'Windstorm'
-//   };
-   heroes=HEROES;
+  heroes: Hero[];
+  //heroes = HEROES;
 
   selectedHero: Hero;
 
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
-  constructor() { }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+
+    this.getHeroes();
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
