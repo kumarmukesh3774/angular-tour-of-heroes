696980b81c3aada8c632a1e2b9bf07ee89bb791f
kumarmukesh3774 <kumarmukesh3774@gmail.com>
2018-03-14 03:03:50 +0530

    tap into the observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,18 +1,58 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+
+import { catchError, map, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+
+import { Hero } from './hero';
+//import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  //constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
-  /* getHeroes(): Hero[] {
-    return HEROES;
-  }
+    /** Log a HeroService message with the MessageService */
+private log(message: string) {
+  this.messageService.add('HeroService: ' + message);
+}
+
+getHeroes (): Observable<Hero[]> {
+  return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      catchError(this.handleError('getHeroes', []))
+    );
+}
+    /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
  */
-getHeroes(): Observable<Hero[]> {
-  return of(HEROES);
+private handleError<T> (operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
 }
-}
+
+  }
+
+/*   getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id)); 
+  } */
